dfe654d251f42fe4f7082ea23f293866d62f2b2a
jrestrada <jramiroem@gmail.com>
2022-06-10 18:12:28 -0700

    1. Fixed some terminology for clarity, 2. Added Database 3. Separated Battle Status print from Selection, Fixed exceptions when selection is not valid

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,8 @@
+#!/usr/local/bin/python3
+
 import gc
 import random
-import typechart as tc
+import pkmdatabase as db
 import numpy as np  
 
 class Pokemon:
@@ -33,7 +35,7 @@ class Pokemon:
 class Trainer:
     def __init__(self, name):
         self.name = name
-        self.pokemons = []
+        self.team = []
 
     def selectTeam(self, d):
         keys = random.sample(list(d), 6)
@@ -43,21 +45,15 @@ class Trainer:
         _team = []
         for names, types in sample.items():
             _team.append(Pokemon(names, types))
-        self.pokemons = _team
+        self.team = _team
          
-class Attack:
-    def __init__(self, name, type, power):
-        self.name = name
-        self.type = type
-        self.power = power
-
 class PokemonGame:
     def __init__(self, trainers):
         self.player1 = trainers[0]
-        self.trainers = trainers
-        self.player2 = None
-        self.p1_pokedex = self.player1.pokemons
-        self.p2_pokedex = self.player2.pokemons
+        self.rivals = trainers[1:]
+        self.player2 = trainers[1]
+        self.p1_pokedex = self.player1.team
+        self.p2_pokedex = self.player2.team
         self.p1_pokemon = None
         self.p2_pokemon = None
         self.turns = 0
@@ -79,10 +75,24 @@ class PokemonGame:
         print("Fight! Select your next attack, type a number from 1 - 4:")
         print(attacks)
         print("-"*70)
-        selection = int(input())
         print("\n")
+
+    def selectAttack(attack = "this key var is just here to fix a bug"):
+        validselections = [1,2,3,4]
+        attackSelected = False
+        while attackSelected == False:
+            try:
+                selection = int(input())
+            except ValueError:
+                print("Invalid option! Select again")
+                continue
+            if selection in validselections:    
+                attackSelected == True
+                break
+            else:
+                print("Invalid option! Select again")
         return selection - 1
-                    
+
 #Function to iterate through all instances of a class.
 def get_all_instances(of_class):
     _instances = []
@@ -93,30 +103,14 @@ def get_all_instances(of_class):
 
 #Instantiation of trainers.
 player1 = Trainer("Ash")
-trainer1 = Trainer("Brock")
-trainer2 = Trainer("Misty")
-trainer3 = Trainer("Team Rocket's James")
-trainer4 = Trainer("Team Rocket's Jessie")
+rival1 = Trainer("Brock")
+rival2 = Trainer("Misty")
+rival3 = Trainer("Team Rocket's James")
+rival4 = Trainer("Team Rocket's Jessie")
 all_trainers = get_all_instances(Trainer) 
 
-#Instantiation of attacks.
-scratch = Attack("Scratch", "NORMAL", 40)
-cut = Attack("Cut", "NORMAL", 50)
-strength = Attack("Strengh", "NORMAL", 80)
-skull_bash = Attack("Skull Bash", "NORMAL",130)
-solar_beam = Attack("Solar Beam", "GRASS", 120)
-seed_bomb = Attack("Seed Bomb", "GRASS", 80)
-thunderbolt = Attack("Thunderbolt", "ELECTRIC", 90)
-spark = Attack("Spark", "ELECTRIC", 65)
-rock_blast = Attack("Rock Blast", "ROCK", 25)
-double_edge = Attack("Double Edge", "ROCK", 120)
-flame_thrower = Attack("Flame Thrower", "FIRE", 90)
-inferno = Attack("Inferno", "FIRE", 100)
-fly = Attack("Fly", "FLYING", 90)
-aereal_ace = Attack("Aereal Ace", "FLYING", 60)
-surf = Attack("Surf", "WATER", 90)
-waterfall = Attack("Water Fall", "WATER", 80)
-all_attacks = get_all_instances(Attack)
+#Instantiation of all attacks in database file.
+all_attacks = get_all_instances(db.Attack)
 
 #Group attacks of the same type into attack_types dictionary.
 attack_dict = {}
@@ -126,15 +120,12 @@ for i in range(0,len(all_attacks)):
     else:
         attack_dict[all_attacks[i].type] += [all_attacks[i]]
 
-#All pokemons available.
-pokemon_dict = {'Charmander': 'FIRE', 'Bulbasaur': 'GRASS', 'Squirtle': 'WATER', 'Pikachu': 'ELECTRIC', 'Geodude': 'ROCK', 'Pidgey': 'FLYING'}
-
-#Add 6 random Pokemons to all trainers.
+#Add 6 random pokemon to all trainers.
 for trainer in all_trainers:
-    trainer.selectTeam(pokemon_dict)
+    trainer.selectTeam(db.pokemon_dict)
     
-    #Add attacks to each of the trainer's pokemons.
-    for pokemon in trainer.pokemons:
+    #Add attacks to each of the trainer's pokemon.
+    for pokemon in trainer.team:
         pokemon.addAttacks(attack_dict)
 
 #Function to print   
@@ -142,17 +133,17 @@ def printPokedexes():
     for i in range(0,len(all_trainers)):
         print("{name}s Pokedex:".format(name = all_trainers[i].name))
         
-        for j in range(0,len(all_trainers[i].pokemons)):
-            print("--{pokName}--".format(pokName = all_trainers[i].pokemons[j].name))
-            print("Health = {health}".format(health = all_trainers[i].pokemons[j].health))
+        for j in range(0,len(all_trainers[i].team)):
+            print("--{pokName}--".format(pokName = all_trainers[i].team[j].name))
+            print("Health = {health}".format(health = all_trainers[i].team[j].health))
             
-            for k in range(0,len(all_trainers[i].pokemons[j].attacks)):
-                print(" -{attName}: {attType}, {attPower}".format(attName = all_trainers[i].pokemons[j].attacks[k].name, attType = all_trainers[i].pokemons[j].attacks[k].type, attPower = all_trainers[i].pokemons[j].attacks[k].power))
+            for k in range(0,len(all_trainers[i].team[j].attacks)):
+                print(" -{attName}: {attType}, {attPower}".format(attName = all_trainers[i].team[j].attacks[k].name, attType = all_trainers[i].team[j].attacks[k].type, attPower = all_trainers[i].team[j].attacks[k].power))
             print("\n")    
 
 #Returns efficacy of an attack
 def typeDamageCalc(attackType = "NORMAL", defendingPkmType = "NORMAL"):
-    return tc.typechart[tc.typeindices.index(attackType)][tc.typeindices.index(defendingPkmType)]
+    return db.typechart[db.typeindices.index(attackType)][db.typeindices.index(defendingPkmType)]
 
 #Returns the total damage an attack inflicts.
 def damage(attackingPkm, defendingPkm, attackIndex):
@@ -162,24 +153,29 @@ def damage(attackingPkm, defendingPkm, attackIndex):
        
 def playGame():
     game = PokemonGame(all_trainers)
-    
     gameOver = False
+
     while gameOver == False:
-        
-        game.player2 = game.trainers[1]
+        game.player2 = game.rivals[0]
         game.p1_pokemon = game.p1_pokedex[0]
         game.p2_pokemon = game.p2_pokedex[0]
 
-        selection = game.printBattleStatus()
+        game.printBattleStatus()
+        selection = game.selectAttack()
         game.p2_pokemon.decreaseHealth(damage(game.p1_pokemon, game.p2_pokemon, selection))
     
         if game.p2_pokemon.health == 0:
             game.p2_pokedex.pop(0)
         
         if game.p2_pokedex == []:
-            game.trainers.pop(1)
+            game.rivals.pop(0)
 
         if game.player2 == game.player1:
             gameOver = True
 
-playGame()
+
+def main():
+    playGame()
+
+if __name__ == "__main__":
+    main()
